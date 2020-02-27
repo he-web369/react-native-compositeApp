53c4a1944a2694984048c35ddf85a2612ee06e99
he-web <897940731@qq.com>
2020-02-28 03:44:10 +0800

    添加了个登录定时器

--- src/pages/Profile/Login.js
+++ src/pages/Profile/Login.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react'
 import { Text, View,StyleSheet,Dimensions,TextInput,TouchableOpacity } from 'react-native'
 
 export default class Login extends Component{
+    state={
+        time:30,
+        showTime:false
+    }
+
+    timeOut=()=>{
+        if(this.state.time!==30)return
+        this.setState({showTime:true})
+        this.timer&&clearInterval(this.timer)
+        this.timer=setInterval(() => {
+            this.setState({time:this.state.time-1})
+            if(this.state.time===0){
+                this.setState(({showTime:false,time:30}))
+                clearInterval(this.timer)
+            }
+        }, 1000)
+
+    }
     render(){
        return  (
             <View>
@@ -31,8 +49,12 @@ export default class Login extends Component{
                 borderColor:'rgba(255,165,0,.7)'
                 }}>
                     <Text style={{textAlignVertical:'center'}}>验证码：</Text>
-                    <TextInput style={{flex:1}} maxLength={16}  placeholder="请输入验证码" 
+                    <TextInput style={{flex:1}} maxLength={6}  placeholder="请输入验证码" 
                     underlineColorAndroid="rgba(255,165,0,.7)"/>
+                    <TouchableOpacity onPress={this.timeOut}
+                        style={{width:130,backgroundColor:'rgba(0,0,0,.2)',borderWidth:1,borderRadius:25,justifyContent:'center',paddingHorizontal:5}}
+                    ><Text style={this.state.showTime?{textAlign:'center',fontSize:12,color:'rgba(0,0,0,.3)'}:{textAlign:'center',fontSize:12}}>
+                        {this.state.showTime?this.state.time+"秒后可再次发送":"点击发送验证码"}</Text></TouchableOpacity>
                 </View>
                 <TouchableOpacity style={{margin:10,
                 padding:10,borderWidth:2,
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
-import { Text, View,StyleSheet,Dimensions } from 'react-native'
+import { Text, View ,Alert} from 'react-native'
 import { Avatar } from 'react-native-elements'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import AIcon from 'react-native-vector-icons/AntDesign'
 
 export default class Profile extends Component{
-
+    goLogin=()=>{
+        Alert.alert(
+            "提示信息",
+            "确定退出吗？",
+            [
+                {text:'确定',onPress:()=>this.props.navigation.navigate('login')},
+                {text:'取消',onPress:()=>{},style:'cancel'}
+            ]
+        )
+    }
     render(){
         const {navigation}=this.props
        return  (
@@ -37,7 +46,9 @@ export default class Profile extends Component{
                     <AIcon name="setting" size={24}/>
                     <Text>设置</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={{margin:10,
+                <TouchableOpacity 
+                onPress={this.goLogin}
+                style={{margin:10,
                 padding:20,borderWidth:2,
                 borderRadius:30,
                 borderColor:'rgba(255,165,0,.7)'}}>
